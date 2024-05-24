7acc3b4b7884ab5508281ba3ed44ad99a418745e
Simon <simon.goy@etu.univ-smb.fr>
2024-05-24 09:21:17 +0200

    Remarque RgmContext.cs
    Ton mdp Ethan... ton mdp...

--- RGMapi/Models/EntityFramework/RgmContext.cs
+++ RGMapi/Models/EntityFramework/RgmContext.cs
@@ -19,7 +19,9 @@ public partial class RgmContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Server=localhost;port=5432;Database=RGM; uid=postgres; password=admin;");
+        => optionsBuilder.UseNpgsql("Server=localhost;port=5432;Database=RGM; uid=postgres; password=admin;"); 
+        // Mmmmm... pas sûr que mettre son mdp comme ça à la vue de tous soit une super idée... Pense à checker la doc de microsoft.
+        // Et passe pas par les logs admin et encore moins de ceux de base pour ton application !
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
